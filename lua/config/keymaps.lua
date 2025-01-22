972ec6d23adc3fe3fc403292424eb4ca96465289
proofer <1290776+proofer@users.noreply.github.com>
2025-01-22 15:57:07 -0800

    Snacks picker

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -20,12 +20,22 @@ map('n', '<A-S-l>', '<cmd>BufferLineMoveNext<CR>',                      { desc =
 
 map('n', 'gh', function() require('config.util').open_github_url() end, { desc = 'Go to gitub repo under cursor' })
 map('n', '<A-d>', function() require('config.util').daily_note() end,   { desc = "Open today's daily note" })
+
 local path = '~/Documents/NotesVault' -- directory in which to live_grep with the following keymap
 map('n', '<leader>sn', function ()
     vim.cmd('cd '..path)
+    local got_picker = false
     if pcall(require, 'telescope') then
+        got_picker = true
         require('telescope.builtin').live_grep( { root = false } )
-    elseif pcall(require, 'fzf-lua') then
-        require('fzf-lua').live_grep({ cwd = vim.fn.getcwd() })  -- not tested
+    elseif pcall(require, 'fzf-lua') then got_picker = true
+        require('fzf-lua').live_grep({ cwd = vim.fn.getcwd() })
+    elseif pcall(require, 'snacks') then
+        local snacks = require('snacks')
+        if snacks.picker and type(snacks.picker) == "table" then
+            got_picker = true
+            snacks.picker.grep()
+        end
     end
-end,  { desc = 'Grep (' .. path:match('([^/]+)$') .. ')' })
+    if not got_picker then print('No telescope, fzf-lua, or snacks.picker found') end
+end,                                                                    { desc = 'Grep (' .. path:match('([^/]+)$') .. ')' })
